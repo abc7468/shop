17352f880db73e7f771b686e1affd0d0e5e07e31
SeongHyun <abc7468@naver.com>
2022-10-03 21:44:01 +0900

    feat : Order Item Entity

--- src/main/java/jpabook/shop/domain/OrderItem.java
+++ src/main/java/jpabook/shop/domain/OrderItem.java
@@ -25,4 +25,14 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
