ddf08f1d3dde7eb1c74c3bd6312c5a7d00de9248
SeongHyun <abc7468@naver.com>
2022-10-03 18:05:37 +0900

    modify : 필요없는 모듈 import 제거

--- src/test/java/jpabook/shop/service/MemberServiceTest.java
+++ src/test/java/jpabook/shop/service/MemberServiceTest.java
@@ -3,10 +3,8 @@ package jpabook.shop.service;
 import jpabook.shop.domain.Member;
 import jpabook.shop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
