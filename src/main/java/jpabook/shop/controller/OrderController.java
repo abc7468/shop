536363b4a73f9e1c0627fdae55cc7e32ba2ee2ae
SeongHyun <abc7468@naver.com>
2022-10-06 22:37:58 +0900

    feat : item order

---
+++ src/main/java/jpabook/shop/controller/OrderController.java
@@ -0,0 +1,42 @@
+package jpabook.shop.controller;
+
+import jpabook.shop.domain.Member;
+import jpabook.shop.domain.item.Item;
+import jpabook.shop.service.ItemService;
+import jpabook.shop.service.MemberService;
+import jpabook.shop.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
